c544fdbf55a6618c8dd30ffa7819c30937279597
Zoe <dadac123@gmail.com>
2015-10-31 19:53:52 +0800

    diary-server

---
+++ diary-server.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import time
+
+'''
+    Simple udpp socketserver
+    Zoe Jane (dadac123@gmail.com)
+'''
+
+import socket
+import sys
+
+HOST = '' # Symbolic name meaning all available interfaces
+PORT = 8888 # Arbitary non-privileged port
+
+# Datagram(udp) socket
+try:
+    s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg:
+    print 'Failed to create socket. Error Code : '+str(msg[0])+' Message ' +msg[1]
+    sys.exit()
+
+# Bind socket to local host and port
+try:
+    s.bind((HOST,PORT))
+except socket.error, msg:
+    print 'Bind failed. Error Code: '+str(msg[0])+' Message '+msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+#now keep taling with the client
+while 1:
+    # receive data from client(data, addr)
+    d=s.recvfrom(1024)
+    data=d[0]
+    addr=d[1]
+
+    if not data:
+        break
+
+    reply='OK...'+data
+
+    s.sendto(reply,addr)
+    print 'time.strftime('%Y/%m/%d')+Message['+addr[0]+':'+str(addr[1])+']-'+data.strip()
+
+s.close()
